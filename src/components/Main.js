e636fb362830bf03897aeb84766d672e988d1609
lixin <594232080@qq.com>
2017-12-20 11:31:57 +0800

    init project

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,31 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/App.scss');
+
+//获取图片相关的数据
+var imageDatas = require('../data/imagedatas.json');
+//利用自执行函数，将图片信息转成图片URL路径信息
+imageDatas=(function genImageURL(imageDataArr){//生成图片文件的路径
+  for(var i= 0,j=imageDataArr.length;i<j;i++){
+    var singleImageDta =imageDataArr[i];
+    singleImageDta.imageURL=require('../images'+singleImageDta.fileName);
+    imageDataArr[i]= singleImageDta.imageURL;
+  }
+  return imageDataArr;
+})(imageDatas);
+
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+      <section className="stage">
+        <section className="img-sec"></section>
+        <nav className="controller-nav">
+
+        </nav>
+      </section>
     );
   }
 }
